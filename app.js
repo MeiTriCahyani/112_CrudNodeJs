2b75e8cd48f5e697af2534eaadbba6bbb7f681e1
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2024-11-07 09:51:31 +0700

    layout

--- app.js
+++ app.js
@@ -3,14 +3,31 @@ const todoRoutes = require('./routes/tododb.js');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
+const db = require('./database/db');
+const expressLayout = require('express-ejs-layouts');
+
+app.use(expressLayout);
 app.use(express.json());
 app.use('/todos', todoRoutes);
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        layout: 'layouts/main-layout'
+    });
 });
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', {
+        layout: 'layouts/main-layout'
+    });
+});
+app.get('/todo-view', (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
 });
 app.use((req, res) => {
     res.status(404).send('404 - Page Not Found');
