0a97054d0411cd4f92f75259aab545ae8945679e
TB1112 <your_mei.tri.ft21@mail.umy.ac.id>
2024-12-05 09:28:24 +0700

    STYLING_PAWC_112

--- app.js
+++ app.js
@@ -1,37 +1,77 @@
 const express = require('express');
-const todoRoutes = require('./routes/tododb.js');
-const app = express();
+//const todoRoutes = require('./routes/todo.js'); 
+const session = require('express-session');
 require('dotenv').config();
-const port = process.env.PORT;
-const db = require('./database/db');
-const expressLayout = require('express-ejs-layouts');
+const expressLayouts = require('express-ejs-layouts');
 
-app.use(expressLayout);
+const db = require('./database/db');
+const authRoutes = require('./routes/authRoutes');
+const todoRoutes = require('./routes/tododb.js');
+const { isAuthenticated } = require('./middlewares/middleware.js');
+
+const app = express();
+const port = process.env.PORT;
+
+
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/todos', todoRoutes);
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false } // Set ke true jika menggunakan HTTPS
+}));
+
+// View Engine Configuration
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
-    res.render('index', {
-        layout: 'layouts/main-layout'
-    });
+app.set('layout', 'layouts/main-layout');
+
+// Middleware untuk menambahkan `user` ke semua tampilan
+app.use((req, res, next) => {
+    res.locals.user = req.session.userId ? { id: req.session.userId } : null;  // Periksa session
+    next();
 });
-app.get('/contact', (req, res) => {
-    res.render('contact', {
-        layout: 'layouts/main-layout'
-    });
+// Static FIles
+app.use(express.static('public'));
+// Layout Middleware
+app.use(expressLayouts);
+// Authentication and Routes
+app.use('/',authRoutes);
+app.use('/todos', todoRoutes);
+
+// Routes with Middleware
+app.get('/', isAuthenticated, (req, res) => {
+    res.render('index');
 });
-app.get('/todo-view', (req, res) => {
+
+app.get('/contact', isAuthenticated, (req, res) => {
+    res.render('contact');
+});
+
+app.get('/todo-view', isAuthenticated, (req, res) => {
     db.query('SELECT * FROM todos', (err, todos) => {
         if (err) return res.status(500).send('Internal Server Error');
-        res.render('todo', {
-            layout: 'layouts/main-layout',
-            todos: todos
-        });
+        res.render('todo', { todos: todos });
     });
 });
+
+// Rute untuk logout
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            return res.status(500).send('Failed to log out');
+        }
+        res.redirect('/login');
+    });
+});
+
+
 app.use((req, res) => {
     res.status(404).send('404 - Page Not Found');
 });
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
